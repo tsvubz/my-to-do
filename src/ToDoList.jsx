037d665b97a8b7c59c99f42e7520f914d28bee09
Terence Svubure <tsvubure@gmail.com>
2024-06-09 18:37:23 +0200

    added functionality to add delete move task (#3)

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -7,22 +7,40 @@ function ToDoList() {
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
-        
+        setNewTask(event.target.value);
     }
 
     function addTask(){
-
+        if(newTask.trim() !== ""){
+            setTasks(tasks => [...tasks, newTask]);
+            setNewTask("");
+        }
     }
 
     function deleteTask(index){
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index){
 
+        if(index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+
     }
 
     function moveTaskDown(index){
+
+        if(index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = 
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
 
     }
 
